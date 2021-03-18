57ebf9829e22c7449152a7af088f845bc6dd773e
Taylor Newton <taylor.newton@TaylorNewtonMBP.attlocal.net>
2021-03-18 15:40:35 -0500

    updated deployment instructions

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,5 +6,13 @@ sudo npm install
 # install process manager
 sudo npm install pm2 -g
 
-# Stop any currently running processes
+# Stop any currently running processes (if running from last deployment)
 sudo pm2 kill
+
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --update
+
+# Copy .env file from S3
+sudo /usr/local/bin/aws s3 cp s3://bon-appetit-api-secrets/.env .env
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,8 +1,5 @@
 cd /home/ubuntu/api
 
-# create new env variable with mongo url
-echo "MONGO_URL=\"fake value\"" | sudo tee .env
-
 # build and run the server
 sudo npm run build
 sudo pm2 start build/app.js
